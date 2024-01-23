8cb3d24d379f7c78fce73483d71027c36067959f
xieyuhua <1130107545@qq.com>
2024-01-23 08:53:21 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,62 @@
+/*
+ * @Author: "xieyuhua" "1130
+ * @Date: 2024-01-19 11:20:38
+ * @LastEditors: "xieyuhua" "1130
+ * @LastEditTime: 2024-01-19 11:51:12
+ * @FilePath: \game20240119\js\data.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+    var personArray = new Array;
+
+   personArray.push({
+            id: 0,
+            image :"img/tx.png",
+            thumb_image :"img/tx.png",
+            name: "544"
+        });
+
+
+    
+        personArray.push({
+            id: 1,
+            image :"img/tx1.png",
+            thumb_image :"img/tx1.png",
+            name: "5"
+        });
+
+
+    
+        personArray.push({
+            id: 2,
+            image :"img/tx2.png",
+            thumb_image :"img/tx2.png",
+            name: "3"
+        });
+
+
+    
+        personArray.push({
+            id: 3,
+            image :"img/tx3.png",
+            thumb_image :"img/tx3.png",
+            name: "7"
+        });
+
+
+    
+        personArray.push({
+            id: 5,
+            image :"img/tx4.png",
+            thumb_image :"img/tx4.png",
+            name: "57"
+        });
+
+
+    
+        personArray.push({
+            id: 6,
+            image :"img/tx5.png",
+            thumb_image :"img/tx5.png",
+            name: "585-"
+        });
+
